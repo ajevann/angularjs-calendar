15c25b298ffd8888df9d7c5c5ff4642dabb5e203
Alex Vann <ajevann@gmail.com>
2014-10-01 16:49:11 -0400

    Button - Directive interaction working, albeit buggy.

--- Original/scripts/actions.js
+++ Original/scripts/actions.js
@@ -1,7 +1,6 @@
 function highlightAll(number){
 	var id = '.' + number;
 	$(id).each(function( i ){
-		console.log('hit ' + $( this ).css( "border" ));
 		$( this ).css( "border", "3px solid #000" );
 	});
 }
@@ -9,7 +8,6 @@ function highlightAll(number){
 function unhighlightAll(number){
 	var id = '.' + number;
 	$(id).each(function( i ){
-		console.log('hit ' + $( this ).css( "border" ));
 		$( this ).css( "border", "1px solid #ddd" );
 	});
 }
